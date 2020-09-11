601306a7d06fc80a71ea21052217bd3f24a0be1e
65421465 <veronika.uhrinova@student.tuke.sk>
2020-09-11 14:43:19 +0200

    design rows

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import requests from './requests'
 
 function App() {
   return (
-    <div className="App">
-      <Row  title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row  title="Trending now" fetchUrl={requests.fetchTrending}/>
-
+    <div className="app">
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
